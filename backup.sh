facb779683548319afb54f5a97e3f26358c0894e
Vincent Boutour <vincent.boutour@gmail.com>
2015-12-12 16:32:11 +0100

    Splitting in two containers

--- backup.sh
+++ backup.sh
@@ -5,9 +5,14 @@ BACKUP_DIR=${2:-`realpath ./`}
 
 echo Backuping ${WORDPRESS_NAME} Wordpress data container into ${BACKUP_DIR}
 
-docker run -d \
-  --name ${WORDPRESS_NAME}_backup \
+docker run --rm \
   -v ${BACKUP_DIR}:/usr/src \
   --volumes-from ${WORDPRESS_NAME}_volumes \
   alpine:latest \
-  tar cvf /usr/src/${WORDPRESS_NAME}_backup_mysql.tar /var/lib/mysql /var/www/localhost
+  tar cvf /usr/src/${WORDPRESS_NAME}_backup_mysql.tar -C /var/lib/mysql .
+
+docker run --rm \
+  -v ${BACKUP_DIR}:/usr/src \
+  --volumes-from ${WORDPRESS_NAME}_volumes \
+  alpine:latest \
+  tar cvf /usr/src/${WORDPRESS_NAME}_backup_www.tar -C /var/www/localhost .
--- restore.sh
+++ restore.sh
@@ -5,11 +5,14 @@ BACKUP_DIR=${2:-`realpath ./`}
 
 echo Restoring ${WORDPRESS_NAME} Wordpress data container from ${BACKUP_DIR}
 
-docker run -d \
-  -name ${WORDPRESS_NAME}_restore \
+docker run --rm \
   -v ${BACKUP_DIR}:/usr/src \
   --volumes-from ${WORDPRESS_NAME}_volumes \
   alpine:latest \
-  tar xvf /usr/src/${WORDPRESS_NAME}_backup.tar -C /tmp \
-  && cp -r /tmp/var/lib/mysql /var/lib/mysql \
-  && cp -r /tmp/var/www/localhost /var/www/localhost
+  tar xvf /usr/src/${WORDPRESS_NAME}_backup_mysql.tar -C /var/lib/mysql
+
+docker run --rm \
+  -v ${BACKUP_DIR}:/usr/src \
+  --volumes-from ${WORDPRESS_NAME}_volumes \
+  alpine:latest \
+  tar xvf /usr/src/${WORDPRESS_NAME}_backup_www.tar -C /var/www/localhost

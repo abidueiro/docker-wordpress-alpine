4c1d268bc65cb2b65784caa17e69a68c3f00926e
Vincent Boutour <ViBiOh@users.noreply.github.com>
2015-12-28 18:00:40 +0100

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -18,7 +18,7 @@ docker run --rm \
   --volumes-from ${DATA_CONTAINER} \
   alpine:latest \
   sh -c 'tar xvzf /usr/src/'${CONTAINER_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp \
-  && rm -rf '${MYSQL_DIR}' '${WWW_DIR}' \
+  && rm -rf '${MYSQL_DIR}'/* '${WWW_DIR}'/* \
   && tar xvf /tmp/www.tar -C '${WWW_DIR}' \
   && tar xvf /tmp/mysql.tar -C '${MYSQL_DIR}
 

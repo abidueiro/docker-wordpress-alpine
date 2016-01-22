20e9b14bcda485d83161af3d2791e613f65dd810
Vincent Boutour <vincent.boutour@gmail.com>
2016-01-22 22:23:42 +0000

    Fixing scripts

--- backup.sh
+++ backup.sh
@@ -8,15 +8,11 @@ fi
 CONTAINER_NAME=${1:-wordpress}
 BACKUP_SUFFIX=${2:-backup}
 BACKUP_DIR=${3:-`realpath ./`}
-MYSQL_CONTAINER=${4:-${CONTAINER_NAME}_mysql}
-DATA_CONTAINER=${5:-${CONTAINER_NAME}_data}
 
 MYSQL_DIR=/var/lib/mysql
 WWW_DIR=/var/www/localhost
 
 echo Backuping ${CONTAINER_NAME} containers into ${BACKUP_DIR} with ${BACKUP_SUFFIX}
-
-docker stop ${MYSQL_CONTAINER}
 
 docker run --rm \
   -v ${BACKUP_DIR}:/usr/src \
@@ -25,5 +21,3 @@ docker run --rm \
   sh -c 'tar cf /tmp/mysql.tar -C '${MYSQL_DIR}' --exclude mysqld.sock --exclude *.pid . \
   && tar cf /tmp/www.tar -C '${WWW_DIR}' . \
   && tar czf /usr/src/'${CONTAINER_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp mysql.tar www.tar'
-
-docker start ${MYSQL_CONTAINER}
--- restore.sh
+++ restore.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 
 if [ "${#}" -eq 0 ]; then
-  echo Usage: restore.sh [name] [suffix] [directory]
+  echo Usage: restore.sh [container] [name] [suffix] [directory]
   exit
 fi
 
-CONTAINER_NAME=${1:-wordpress}
-BACKUP_SUFFIX=${2:-backup}
-BACKUP_DIR=${3:-`realpath ./`}
-MYSQL_CONTAINER=${4:-${CONTAINER_NAME}_mysql}
-DATA_CONTAINER=${5:-${CONTAINER_NAME}_data}
+DATA_CONTAINER=${1:-wordpress}
+BACKUP_NAME=${2:-wordpress}
+BACKUP_SUFFIX=${3:-backup}
+BACKUP_DIR=${4:-`realpath ./`}
 
 MYSQL_DIR=/var/lib/mysql
 WWW_DIR=/var/www/localhost
 
-echo Restoring ${CONTAINER_NAME} containers from ${BACKUP_DIR} with ${BACKUP_SUFFIX}
-
-docker stop ${MYSQL_CONTAINER}
+echo Restoring ${BACKUP_NAME} containers from ${BACKUP_DIR} with ${BACKUP_SUFFIX}
 
 docker run --rm \
   -v ${BACKUP_DIR}:/usr/src \
   --volumes-from ${DATA_CONTAINER} \
   alpine:latest \
-  sh -c 'tar xzf /usr/src/'${CONTAINER_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp \
+  sh -c 'tar xzf /usr/src/'${BACKUP_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp \
   && rm -rf '${MYSQL_DIR}'/* '${WWW_DIR}'/* \
   && tar xf /tmp/www.tar -C '${WWW_DIR}' \
   && tar xf /tmp/mysql.tar --exclude mysqld.sock --exclude *.pid -C '${MYSQL_DIR}
-
-docker start ${DATA_CONTAINER} ${MYSQL_CONTAINER}

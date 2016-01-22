557e5a24f51cfdbbc7fe0a80241fe0b0a5592bb3
Vincent Boutour <vincent.boutour@gmail.com>
2016-01-22 23:37:24 +0100

    Fixing backup

--- backup.sh
+++ backup.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 if [ "${#}" -eq 0 ]; then
-  echo Usage: backup.sh [name] [suffix] [directory]
+  echo Usage: backup.sh [container] [name] [suffix] [directory]
   exit
 fi
 
-CONTAINER_NAME=${1:-wordpress}
-BACKUP_SUFFIX=${2:-backup}
-BACKUP_DIR=${3:-`realpath ./`}
+DATA_CONTAINER=${1:-wordpress}
+BACKUP_NAME=${2:-wordpress}
+BACKUP_SUFFIX=${3:-backup}
+BACKUP_DIR=${4:-`realpath ./`}
 
 MYSQL_DIR=/var/lib/mysql
 WWW_DIR=/var/www/localhost
 
-echo Backuping ${CONTAINER_NAME} containers into ${BACKUP_DIR} with ${BACKUP_SUFFIX}
+echo Backuping ${BACKUP_NAME} containers into ${BACKUP_DIR} with ${BACKUP_SUFFIX}
 
 docker run --rm \
   -v ${BACKUP_DIR}:/usr/src \
@@ -20,4 +21,4 @@ docker run --rm \
   alpine:latest \
   sh -c 'tar cf /tmp/mysql.tar -C '${MYSQL_DIR}' --exclude mysqld.sock --exclude *.pid . \
   && tar cf /tmp/www.tar -C '${WWW_DIR}' . \
-  && tar czf /usr/src/'${CONTAINER_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp mysql.tar www.tar'
+  && tar czf /usr/src/'${BACKUP_NAME}'_'${BACKUP_SUFFIX}'.tar.gz -C /tmp mysql.tar www.tar'
